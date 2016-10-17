68b5903a1ce51e3bc729c571ecc22bbf6d70a456
joel <joel@localhost.localdomain>
2016-10-17 06:48:21 +0300

    adding of the @login_required decorator

--- app.py
+++ app.py
@@ -74,6 +74,10 @@ def login():
 
 @app.route('/logout')
 @login_required
+def logout():
+    logout_user()
+    flash("YOu've been loged out!!", "success")
+    return redirect(url_for('index'))
 
 
 
